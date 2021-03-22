5fea7185e9ff75750cd1133b66139c57ff623c58
Fernando <averaldofh@gmail.com>
2021-03-22 18:03:02 -0400

    Update csv_to_mp3.py

--- csv_to_mp3.py
+++ csv_to_mp3.py
@@ -1,53 +1,54 @@
 #!/usr/bin/env python3
 
-'''
-# csv to mp3 #
+    '''
+    # csv to mp3 #
 
-### What it does: ###
+    ### What it does: ###
 
-This simple script takes a .csv list of popular song titles and artist names
-and searches youtube for that song, grabs the first link that isn't an ad and
-downloads the video and converts it to mp3 using youtube-dl/ffmpeg.
+    This simple script takes a .csv list of popular song titles and artist names
+    and searches youtube for that song, grabs the first link that isn't an ad and
+    downloads the video and converts it to mp3 using youtube-dl/ffmpeg.
 
-So you can listen to these songs off the [internet] grid!
+    So you can listen to these songs off the [internet] grid!
 
-### Why: ###
-Sometimes you don't have an internet connection and this is is an easy & lazy
-way to download a couple of offline songs w/o worrying about torrenting
-individual songs or using youtube2mp3 for each individual song.
+    ### Why: ###
+    Sometimes you don't have an internet connection and this is is an easy & lazy
+    way to download a couple of offline songs w/o worrying about torrenting
+    individual songs or using youtube2mp3 for each individual song.
 
-Not recommended for building a large library - intended for getting a few songs to
-drag to my watch and dumb phone so I can listen to a couple new songs while out
+    Not recommended for building a large library - intended for getting a few songs to
+    drag to my watch and dumb phone so I can listen to a couple new songs while out
 
-*Not to be used to download music illegally!*
-*Please follow local copyright law & support artists!!*
-
-
-
-### *Instructions:* ###
-1. Install ffmpeg (https://github.com/adaptlearning/adapt_authoring/wiki/Installing-FFmpeg)
-2. Install youtube_dl, pandas, and bs4 with "pip3 install packagename"
-3. Create a songs.csv in the same directory as this auto_yt_dl with a "song" and "artist" field
-   (see current songs.csv for example of what it should look like)
-4. Run this "python csv_to_mp3.py" (may take about a minute per song)
-5. Drag the music to your offline device, and enjoy!
-
-Note: It really only works for fairly popular songs that are on youtube.
+    *Not to be used to download music illegally!*
+    *Please follow local copyright law & support artists!!*
 
 
-todo:
-- figure out how to get a csv output from spotify
-- maybe worry about output song names?
-- make into .exe?
 
-credit:
-https://stackoverflow.com/questions/30825371/extract-audio-equivalent-for-youtubedl-class
+    ### *Instructions:* ###
+    1. Install ffmpeg (https://github.com/adaptlearning/adapt_authoring/wiki/Installing-FFmpeg)
+    2. Install youtube_dl, pandas, and bs4 with "pip3 install packagename"
+    3. Create a songs.csv in the same directory as this auto_yt_dl with a "song" and "artist" field
+    (see current songs.csv for example of what it should look like)
+    4. Run this "python csv_to_mp3.py" (may take about a minute per song)
+    5. Drag the music to your offline device, and enjoy!
 
-requires:
-ffmpeg - google how to install it and put it in the same directory as your
-all below modules (youtube_dl, pandas, bs4) - install w/ "pip3 install youtube_dl" and etc.
+    Note: It really only works for fairly popular songs that are on youtube.
 
-'''
+
+    todo:
+    - figure out how to get a csv output from spotify
+    - maybe worry about output song names?
+    - make into .exe?
+
+    credit:
+    https://stackoverflow.com/questions/30825371/extract-audio-equivalent-for-youtubedl-class
+
+    requires:
+    ffmpeg - google how to install it and put it in the same directory as your
+    all below modules (youtube_dl, pandas, bs4) - install w/ "pip3 install youtube_dl" and etc.
+
+    '''
+
 import youtube_dl
 import pandas as pd
 import requests
@@ -113,6 +114,7 @@ def main():
         i+=1
     print('Rename files to avoid ugly underscores')
     names('Downloaded_mp3')
+    input("Done! Press enter to exit")
 
 if __name__ == "__main__":
     main()
