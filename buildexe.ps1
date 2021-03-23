f78ba2f7e1f9e33c51df0ee7eb4c011fcea6fd35
averaldofh <averaldofh@gmail.com>
2021-03-23 19:13:55 -0400

    exe build configuration files

---
+++ buildexe.ps1
@@ -0,0 +1,6 @@
+echo "D:\pyve\Scripts\Activate.ps1"
+D:\pyve\Scripts\Activate.ps1
+echo "cd D:\Python\csv_to_mp3\"
+cd D:\Python\csv_to_mp3\
+echo "pyinstaller --noconfirm --onefile --windowed --icon "D:/Python/csv_to_mp3/ico.ico" --clean --log-level "WARN" --add-data "D:/Python/csv_to_mp3/ico.ico;."  "D:/Python/csv_to_mp3/csv_to_mp3_gui.py""
+pyinstaller --noconfirm --onefile --windowed --icon "D:/Python/csv_to_mp3/ico.ico" --clean --log-level "WARN" --add-data "D:/Python/csv_to_mp3/ico.ico;."  "D:/Python/csv_to_mp3/csv_to_mp3_gui.py"
